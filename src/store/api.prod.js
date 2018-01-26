ea898ef81927e5df67c2e8476d33208d909c078c
yinhewang <galaxyw@yinhewangdeMacBook-Pro.local>
2018-01-26 12:32:18 +0800

    更正 生产环境发布域名配置

--- src/store/api.prod.js
+++ src/store/api.prod.js
@@ -1,16 +1,13 @@
 module.exports = {
-  "version": "v1.0.1",
-  /*
-   * @production
-   *
-  "nurseTrainApi": "//nursetrain.prd.317hu.com",
-  "www_form_urlencoded_nurseTrainApi": "//nursetrain.prd.317hu.com",
-  "privilegeApi": "//privilege.317hu.com",
-  "tradeApi": "//tc.prd.317hu.com"
-  */
-  "nurseTrainApi": "//nursetrainuat.317hu.com",
-  "www_form_urlencoded_nurseTrainApi": "//nursetrainuat.317hu.com",
-  "careCentralApi": "//uat.317hu.com",
-  "privilegeApi": "//uatprivilege.317hu.com:8081",
-  "tradeApi": "//tc.uat.317hu.com"
-}
+  version: 'v1.0.1',
+  loginConfig: '//317hu.com/care-central/page/login',
+  mock_nurseTrainApi: '//rap.api.317hu.com/app/mock/17',
+  nurseTrainApi: '//nursetrain.prd.317hu.com',
+  www_form_urlencoded_nurseTrainApi: '//nursetrain.prd.317hu.com',
+  www_form_urlencoded_usercentralApi: '//usercentral.317hu.com',
+  careCentralApi: '//317hu.com',
+  privilegeApi: '//privilege.317hu.com',
+  tradeApi: '//tradecenter.317hu.com',
+  qiniuDomain: '//image.317hu.com',
+};
+
