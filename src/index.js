1b26fc66badda8227b48f080087fae65df9b3977
yinhewang <galaxyw@yinhewangdeMacBook-Pro.local>
2018-04-26 15:57:43 +0800

    切换 到app模式

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import { Provider } from 'react-redux';
 // import createClass from 'create-react-class';
 import { configureStore, history } from './store/configureStore';
 import { saveCache } from './core/_utils/storage';
-import App from '../examples/v2.2.0/views/app';
-// import App from './views/app';
+// import App from '../examples/v2.2.0/views/app';
+import App from './views/app';
 
 // import AnimationExample from './views/AnimationExample';
 
