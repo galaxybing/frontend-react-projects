f4fb4e12bf3122d715a3f836066b0746eda38f30
galaxyw <wangyh@317hu.com>
2018-09-29 00:10:09 +0800

    更新 发布服务器地址
    Change-Id: I8e8b06383d2536ca9f73cdb33d94f64f9812ea62

--- src/store/api.js
+++ src/store/api.js
@@ -12,19 +12,13 @@ var repository = require('../../package.json').name;
 var ver = versionEnv;
 boz['env'] = versionEnv;
 
-if (/^v(\d){1,2}\.(\d){1,2}\.(\d){1,4}$/.test(ver)) {
-  // boz['assetsPublicPathConfig'] = '//resources.317hu.com/' + repository + '/' + ver + '/static/';
-  // ???
-  boz['assetsPublicPathConfig'] = '//resources.317hu.com/' + repository + '/' + ver + '/static/';
-  boz['loginConfig'] = '//317hu.com/care-central/page/login';
-} else if (boz[`RUN_ENV`] === 'dll' || boz[`RUN_ENV`] === 'local') {
+if (boz[`RUN_ENV`] === 'dll' || boz[`RUN_ENV`] === 'local') {
   boz['assetsPublicPathConfig'] = '/static/';
   boz['loginConfig'] = '';
 } else { // dev sit uat
-  // boz['assetsPublicPathConfig'] = '//172.16.150.169:8012/' + repository + '/' + ver + '-' + branchEnv + '/' + repository + '/' + ver + '-' + branchEnv + '/static/';
   // 环境变量 移除
-  boz['assetsPublicPathConfig'] = '//172.16.150.169:8012/' + repository + '/' + branchEnv + '/' + repository + '/' + branchEnv + '/static/';
-  boz['loginConfig'] = '//' + ver + '.317hu.com/care-central/page/login';
+  boz['assetsPublicPathConfig'] = '//fe-pub.317hu.com/' + repository + '/' + branchEnv + '/static/';
+  boz['loginConfig'] = '//hospital.' + ver + '.317hu.com/hospital-admin/317hu-login/login.html';
 }
 
 module.exports = boz;
