3853fe40a1a534330fbadbed2ff9f2a61cdf61f3
galaxyw <wangyh@317hu.com>
2018-09-29 23:16:17 +0800

    新增 source 命令执行
    Change-Id: Ifd2fab1e6c958cac8c170f7db1937930f8379287

--- build/build.sh
+++ build/build.sh
@@ -6,50 +6,7 @@
 # 
 Pub=$1
 echo "----------------------------------"
-
-# 处理发布目录
-checked_git_dir () {
-  local dir=. origin
-  if [ ! -d "$dir/.git" ]; then
-    origin=$(git remote -v | grep origin | head -1 | awk '{print $2}')
-    git init
-    git remote add origin $origin
-  fi
-}
-
-checked_git_status () {
-  local dir=. msg branch_name commit_msg_status
-  if [ -d "$dir/.git" ]; then
-    msg=$(git status | grep 'Changes not staged for commit' | awk '{print $3}')
-    branch_name=$(git branch -v | grep '*' | awk '{print $2}')
-    # echo -e "$env_list"
-    if [ "$msg" = 'staged' ]; then
-      commit_msg_status="...请暂存本地修改？" # 因为，当前为发布服务器源码构建；所以需要本地源码必需暂存
-    else
-      # 统一发布
-      if [ -d "tmp" ];then
-        rm -rf tmp
-      fi
-      mkdir -m 7777 tmp
-      git clone ./ ./tmp
-      rm -rf tmp/.git
-
-      if [ ! -d "pub" ];then
-        mkdir -m 7777 pub
-      fi
-      cd ./pub
-      checked_git_dir
-
-      cp -a ../tmp/* .
-      rm -rf ../tmp
-      git add .
-      git commit -m "$env_list@$commit_msg@$update_node_modules_status"
-      git push -f origin "HEAD:pub-$branch_name"
-      commit_msg_status="...构建完成。"
-    fi
-    echo -e "$commit_msg_status"
-  fi
-}
+# exec ./build/common.sh
 
 update_node_modules_status=1
 
